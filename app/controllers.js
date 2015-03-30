c2ce039d4e7332e019e3cf6c54eb1b2eb8ade3ff
Camilo Rodríguez <rodriguez.camilo@gmail.com>
2015-03-29 22:29:05 -0500

    versión cliente solo terminada

--- app/controllers.js
+++ app/controllers.js
@@ -1,17 +1,25 @@
 'use strict';
 
 /* Controllers */
-
-var aMailServices = angular.module('AMail', ['ngRoute']);
-
+var appServices = angular.module('app', ['ngRoute','ngResource']);
+appServices.constant("configuracion",{"url":"http://localhost:8000"});
+//appServices.constant("configuracion",{"url":"http://www.epsilondx.com/django/index.fcgi"});
 
 // Set up our mappings between URLs, templates, and controllers
-function emailRouteConfig($routeProvider) {
+function appRouteConfig($routeProvider) {
 $routeProvider.
-when('/', {
+when('/dodo', {
 controller: ListController,
 templateUrl: 'list.html'
 }).
+when('/acerca', {
+controller: AcercaController,
+templateUrl: 'acerca.html'
+}).
+when('/', {
+controller: UsuarioRedController,
+templateUrl: 'usuario_red.html'
+}). 
 // Notice that for the detail view, we specify a parameterized URL component
 // by placing a colon in front of the id
 when('/view/:id', {
@@ -22,35 +30,109 @@ otherwise({
 redirectTo: '/'
 });
 }
+appServices.factory('RedFactory', ['$resource', 'configuracion',function($resource,configuracion) {
+return $resource(configuracion.url+'/redes/:id');
+}]);
+appServices.factory('UsuarioFactory', ['$resource', 'configuracion',function($resource,configuracion) {
+return $resource(configuracion.url+'/usuarios/:id');
+}]);
+appServices.factory('UsuarioRedFactory', ['$resource', 'configuracion',function($resource,configuracion) {
+return $resource(configuracion.url+'/usuarios/:idusuario/redes');
+}]);
+
+appServices.factory('Categoria', ['$resource', 'configuracion',function($resource,configuracion) {
+return $resource(configuracion.url+'/usuarios/:id');
+}]);
+
+appServices.config(['$routeProvider', appRouteConfig]);
 
 
-aMailServices.config(['$routeProvider', emailRouteConfig]);
-
-
-var messages = [{
-id: 0, sender: 'jean@somecompany.com', subject: 'Hi there, old friend',
-date: 'Dec 7, 2013 12:32:00', recipients: ['greg@somecompany.com'],
-message: 'Hey, we should get together for lunch sometime and catch up.'
-+'There are many things we should collaborate on this year.'
-}, {
-id: 1, sender: 'maria@somecompany.com',
-subject: 'Where did you leave my laptop?',
-date: 'Dec 7, 2013 8:15:12', recipients: ['greg@somecompany.com'],
-message: 'I thought you were going to put it in my desk drawer.'
-+'But it does not seem to be there.'
-}, {
-id: 2, sender: 'bill@somecompany.com', subject: 'Lost python',
-date: 'Dec 6, 2013 20:35:02', recipients: ['greg@somecompany.com'],
-message: 'Nobody panic, but my pet python is missing from her cage.'
-+"She doesn't move too fast, so just call me if you see her."
-}, ];
-
-// Publish our messages for the list template
-function ListController($scope) {
-$scope.messages = messages;
+function ListController($scope,$http,UsuarioRedFactory) {
+    var datos = UsuarioRedFactory.query({idusuario:1},function() {
+    console.log(datos);
+    });
 }
+function UsuarioRedController($scope,$http,UsuarioRedFactory,RedFactory) {
+    var originales;
+    $scope.nuevared={"url":"","usuario":"","red_social":""};
+    $scope.bAgregar=false;
+    $scope.getRedById=function(id)
+    {
+        var longitud=0;
+        if($scope.redes)
+        {
+            longitud=$scope.redes.length;
+        }
+    
+        var i;
+        for(i=0;i<longitud;i++)
+        {
+            if($scope.redes[i].id==id)
+                return $scope.redes[i];
+        }    
+        return -1;
+    }
+
+    $scope.editar=function(index)
+    {
+    $scope.bGuardar[index]=true;
+
+    
+     
+    }
+    $scope.cancelar=function(index)
+    {
+    // console.log(originales);    
+    $scope.bEditar[index]=true;
+    $scope.bGuardar[index]=false;
+    console.log(originales[index]);
+    console.log($scope.usuarioredes[index]);
+    $scope.usuarioredes[index]=originales[index];    
+    }
+    $scope.borrar=function(index)
+    {
+        $scope.bGuardar.splice(index,1);
+        $scope.bEditar.splice(index,1);
+        $scope.usuarioredes.splice(index,1);
+     
+    }
+    
+    $scope.guardar=function(index)
+    {
+      $scope.bEditar[index]=true;
+      $scope.bGuardar[index]=false;
+      
+    }
+    $scope.crear=function()
+    {
+      $scope.bAgregar=false;
+      $scope.bEditar.push(false);
+      $scope.bGuardar.push(false);
+      $scope.usuarioredes.push($scope.nuevared);
+      $scope.nuevared={"url":"","usuario":"","red_social":""};
+    
+    }
+   
+    UsuarioRedFactory.query({idusuario:1},function(usuarioredes) {
+        $scope.bEditar=new Array(usuarioredes.length);
+        $scope.bGuardar=new Array(usuarioredes.length);
+        $scope.usuarioredes = usuarioredes;
+        originales=usuarioredes;
+        
+    });
+    
+    RedFactory.query(function(redes) {
+        $scope.redes = redes;
+        console.log(redes);
+    });
+        
+}
+
 // Get the message id from the route (parsed from the URL) and use it to
 // find the right message object.
 function DetailController($scope, $routeParams) {
-$scope.message = messages[$routeParams.id];
+
+}
+function AcercaController($scope) {
+
 }
